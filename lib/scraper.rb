ed3841fcb1153aa99811c5c16e1dd647b97b4c9c
Elizabeth Karst <efkarst@outlook.com>
2018-09-15 17:50:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,10 +20,11 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
 
-    doc.css(".main-wrapper .vitals-container .social-icon-container").children.each do |link|
-      {:twitter => link.css("a").attribute("href").value
+    doc.css(".main-wrapper .vitals-container .social-icon-container").collect do |link|
+      {:twitter => link.children.css("a").attribute("href").value
       }
     end
+    
 
     binding.pry
     # => {:twitter=>"http://twitter.com/flatironschool",
